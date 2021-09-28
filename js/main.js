35ca375842b7024289b7868bd611e379eaf3d1ef
SM Boolean <samuelemadrigali1991@gmail.com>
2021-09-28 09:55:49 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+const app = new Vue({
+	el: '#root',
+	data: {
+		emails: []
+	},
+	methods: {
+
+	},
+	mounted() {
+		for ( let i = 1; i <= 10; i++ ) {
+			axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+			.then( (resp) => {
+				this.emails.push(resp.data.response);
+			});
+		}
+	}
+});
